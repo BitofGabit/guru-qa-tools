ea6aca0b326d445dde247dddc30d7adcd9cead86
kabylbekovgabit <kabylbekovgabit@kazdream.kz>
2021-06-22 19:57:26 +0600

    added dependency and methodes

---
+++ src/test/java/SearchTests.java
@@ -0,0 +1,44 @@
+import com.codeborne.selenide.Config;
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selectors.byName;
+import static com.codeborne.selenide.Selenide.*;
+import static com.codeborne.selenide.Selenide.open;
+
+public class SearchTests {
+
+    static Logger logger = LoggerFactory.getLogger(SearchTests.class);
+
+    @BeforeAll
+    static void config(){
+        logger.info("BeforeAll method");
+        Configuration.startMaximized = true;
+        Configuration.browser = "chrome";
+    }
+
+    @BeforeEach
+    void openGooglePage(){
+        logger.info("BeforeEach method");
+        open("https://google.com/");
+    }
+
+    @Test
+    void selenideSearchTest(){
+        logger.info("This is selenide test!");
+        $(byName("q")).setValue("selenide").pressEnter();
+        $("#search").shouldHave(text("selenide.org"));
+    }
+
+    @Test
+    void allureSearchTest(){
+        logger.info("This is allure test!");
+        $(byName("q")).setValue("Allure testops").pressEnter();
+        $("#search").shouldHave(text("qameta.io"));
+    }
+}
