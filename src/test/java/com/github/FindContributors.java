8e9db6c44710b4290d046ac6490a7c424814eb47
kabylbekovgabit <kabylbekovgabit@kazdream.kz>
2021-07-04 21:33:44 +0600

    coded: github end-to-end test, drag and drop test

--- src/test/java/com/github/FindContributors.java
+++ src/test/java/com/github/FindContributors.java
@@ -1,19 +1,36 @@
 package com.github;
 
-import static com.codeborne.selenide.Selectors.byText;
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
 
 public class FindContributors {
 
-    void firstContributorsShouldBeSolntsev(){
-
-        open("https://github.com/selenede/selenede");
-
-        $(".class").setValue("selenide");
-
-        $("").$(byText("Conti"))
-                .closest("").$("li").hover();
+    @BeforeAll
+    static void setup(){
+        Configuration.startMaximized = true;
+        open("https://github.com");
     }
 
+    @Test
+    void firstContributorsShouldBeSolntsev(){
+        // Откройте страницу Selenide в Github
+        $("[data-scoped-placeholder=\"Search\"]")
+                .setValue("selenide").pressEnter();
+        $(".repo-list a").click();
+        // Перейдите в раздел Wiki проекта
+        $("[data-content=\"Wiki\"]").click();
+        // Убедитесь, что в списке страниц (Pages) есть страница SoftAssertions
+        $(".js-wiki-sidebar-toggle-display li:last-child > button").click();
+        $(".js-wiki-sidebar-toggle-display .wiki-more-pages > strong > a")
+                .shouldHave(text("SoftAssertions"));
+        $(".js-wiki-sidebar-toggle-display .wiki-more-pages > strong > a").click();
+        // Проверьте что внутри есть пример кода для JUnit5
+        $("[start=\"3\"] > li").
+                shouldHave(text("Using JUnit5 extend test class:"));
+    }
 }
--- src/test/java/com/theInternetHerokuapp/DragAndDrop.java
+++ src/test/java/com/theInternetHerokuapp/DragAndDrop.java
@@ -1,17 +1,36 @@
 package com.theInternetHerokuapp;
 
+import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Selenide.*;
+
 public class DragAndDrop {
 
-    //ДЗ 1
-    //1. Есть ли разница между $("h1 div"); и $("h1").$("div"); - может ли привести к тому что, поиск найдёт разные элементы?
+    /**
+     *  ДЗ 1
+     *  1. Есть ли разница между $("h1 div"); и $("h1").$("div"); - может ли привести к тому что, поиск найдёт разные элементы?
+     *
+     *  Ответ на ДЗ 1
+     *  Есть разница, в первом одна операция для поиска первого div внутри h1, во втором две операций для того же самого.
+     */
 
-    //Ответ на ДЗ 1
-    //Есть разница, в первом одна операция для поиска первого div внутри h1, во втором две операций для того же самого.
+    @BeforeAll
+    static void setup(){
+        Configuration.startMaximized = true;
+        open("https://the-internet.herokuapp.com/drag_and_drop");
+    }
 
 
-
-
-    void ma(){
+    @Test
+    void dragAndDropTest(){
+        // - Перенесите прямоугольник А на место В
+        $("#column-a").dragAndDropTo("#column-b");
+        // - Проверьте, что прямоугольники действительно поменялись
+        $("#column-a").shouldNotBe(Condition.text("A"));
+        $("#column-b").shouldNotBe(Condition.text("B"));
 
     }
 
