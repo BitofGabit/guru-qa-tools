9da2c5ab595216badb854095da4476dda2ce0c1b
kabylbekovgabit <kabylbekovgabit@kazdream.kz>
2021-07-13 13:55:39 +0600

    added: files for tests for file

---
+++ src/test/java/com/github/testFiles/DownloadFileTests.java
@@ -0,0 +1,27 @@
+package com.github.testFiles;
+
+import com.codeborne.selenide.Configuration;
+import com.github.testFiles.Utils.Files;
+import org.junit.jupiter.api.Test;
+
+import java.io.File;
+import java.io.IOException;
+
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.StringContains.containsString;
+
+public class DownloadFileTests  {
+    @Test
+    void selenideDownloadReadmeTest() throws IOException {
+        Configuration.downloadsFolder = "downloads";
+
+        open("https://github.com/selenide/selenide/blob/master/README.md");
+        File downloadedFile = $("#raw-url").download();
+        String fileContent = Files.readTextFromFile(downloadedFile);
+
+      //assertTrue(fileContent.contains("Selenide = UI Testing Framework powered by Selenium WebDriver"));
+        assertThat(fileContent, containsString("Selenide = UI Testing Framework powered by Selenium WebDriver"));
+    }
+}
