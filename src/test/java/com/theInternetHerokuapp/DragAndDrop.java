b9df096ff0a655e4c2702e0fe1b38b04fb098c77
kabylbekovgabit <kabylbekovgabit@kazdream.kz>
2021-07-13 19:10:43 +0600

    fixed: DragAndDrop and SelenideWikiPage classes

--- src/test/java/com/theInternetHerokuapp/DragAndDrop.java
+++ src/test/java/com/theInternetHerokuapp/DragAndDrop.java
@@ -9,23 +9,23 @@ import static com.codeborne.selenide.Selenide.*;
 
 public class DragAndDrop {
 
-    /**
-     *  ДЗ 1
-     *  1. Есть ли разница между $("h1 div"); и $("h1").$("div"); - может ли привести к тому что, поиск найдёт разные элементы?
+    /** homeWork4 task1
+     *  Сейчас
+     *  Есть ли разница между $("h1 div"); и $("h1").$("div"); - может ли привести к тому что, поиск найдёт разные элементы?
      *
-     *  Ответ на ДЗ 1
+     *  Должно быть
      *  Есть разница, в первом одна операция для поиска первого div внутри h1, во втором две операций для того же самого.
      */
 
     @BeforeAll
     static void setup(){
         Configuration.startMaximized = true;
-        open("https://the-internet.herokuapp.com/drag_and_drop");
     }
 
 
     @Test
     void dragAndDropTest(){
+        open("https://the-internet.herokuapp.com/drag_and_drop");
         // - Перенесите прямоугольник А на место В
         $("#column-a").dragAndDropTo("#column-b");
         // - Проверьте, что прямоугольники действительно поменялись
